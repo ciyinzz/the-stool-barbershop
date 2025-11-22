6c56af8324e36607360eefe3cd2b1b5982c91ae0
Wong Ciyin <ciyinzz@icloud.com>
2025-11-22 16:52:32 +0800

    Fix build issues

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { Link, NavLink } from "react-router-dom";
-import logo from "../assets/logo.png";
+import logo from "../assets/logo-modified.png";
 import DarkModeToggle from "./DarkModeToggle";
 
 export default function Navbar() {
